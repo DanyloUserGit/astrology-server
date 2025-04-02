c1e6dd5352276bc4a28ce272db7fe7a9320af7c8
DanyloUserGit <71781037+DanyloUserGit@users.noreply.github.com>
2025-04-02 20:54:48 +0300

    Update synastry-chart.service.ts

--- src/synastry-chart/synastry-chart.service.ts
+++ src/synastry-chart/synastry-chart.service.ts
@@ -133,11 +133,14 @@ export class SynastryService {
             console.log("Sending email to:", email);
             const transporter = nodemailer.createTransport({
                 host: "mail.adm.tools",
+                port: 587,
                 secure: false,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS,
                 },
+                logger: true, // Додати логування для відправки
+                debug: true,  // Вивести додаткові відомості для діагностики
             });
 
             const mailOptions = {
