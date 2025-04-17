0fcf5d4ea626530968d9ab0ee9870598fa9f36be
danylo <danylom2007.m@gmail.com>
2025-04-17 14:19:10 +0300

    stripe

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './stripe.service';
 import { PaymentController } from './stripe.controller';
+import { SynastryModule } from 'src/synastry-chart/synastry-chart.module';
 
 @Module({
+  imports: [SynastryModule],
   providers: [PaymentService],
   controllers:[PaymentController],
   exports: [PaymentService],
